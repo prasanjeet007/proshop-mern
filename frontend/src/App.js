4788fc150a1136c486dd81e6e5f3849994e4bba4
Prasanjeet Chatterjee <prasanjeetchatterjee28@gmail.com>
2023-12-29 18:48:06 +0530

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>Welcome To ProShop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
-
 export default App;
