e53b48d0baf8e82af20d9879a7e9f0ed1d361c01
Prasanjeet Chatterjee <prasanjeetchatterjee28@gmail.com>
2024-01-26 18:51:16 +0530

    usercontroller userroutes and generatetoken

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5500;
 connectDB();
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
